efb12c9cb81538952ae847500f35f8dffe112393
Zymed <miseso22@gmail.com>
2021-11-15 11:28:55 +0100

    Cinemachine Freelook Camera testing
    Created a placeholder for a player, plane and a cinemachine freelook camera

--- P1-Project/Assets/Scripts/Player.cs
+++ P1-Project/Assets/Scripts/Player.cs
@@ -1,65 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    int hp = 10;
-    int minHp;
-    int maxHp;
+    private int playerMaxHealth = 100;
+    private int playerMinimumHealth = 0;
+    private int playerCurrentHealth;
+    
 
-    Scene activeScene;
-    int sceneNumber;
 
-    bool isDead;
-    // Start is called before the first frame update
     void Start()
     {
-        activeScene = SceneManager.GetActiveScene();
-        sceneNumber = activeScene.buildIndex;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        IsDead();
-        if (isDead)
-        {
-            Restartlvl();
-        }
+        
     }
-
-    void TakeDamage (int dmg)
-    {
-        hp -= dmg;
-    }
-
-    void IsDead()
-    {
-        if (hp < minHp)
-        {
-            isDead = true;
-        } else isDead = false;
-
-    }
-
-    void Restartlvl()
-    {
-        SceneManager.LoadScene(sceneNumber);
-    }
-    private void OnCollisionEnter(Collision other)
-    {
-        Debug.Log("Collisiondetected");
-        if (other.gameObject.CompareTag("Enemy"))
-        {
-            Debug.Log(hp);
-            int dmg = other.gameObject.GetComponent<Enemy>().dmg;
-            TakeDamage(dmg);
-
-        }
-    }
-
-
-
 }
