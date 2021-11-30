265e394805b619c2d892c5d0ba7dbd52d21e76cb
monniiaa <74655535+monniiaa@users.noreply.github.com>
2021-11-30 18:38:06 +0100

    Very cursed movement

--- P1-Project/Assets/Scripts/Camera.cs
+++ P1-Project/Assets/Scripts/Camera.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //input
+    KeyCode leftMouse = KeyCode.Mouse0, rightMouse = KeyCode.Mouse1, middleMouse = KeyCode.Mouse2;
+
+    // Camera variables
+    public float cameraHeight = 1.75f, cameraMaxDistance = 25;
+    readonly float cameraMaxTilt = 90;
+    [Range(0,4)]
+    public float cameraSpeed = 2;
+    float currentPan, currentTilt = 10, currenDistance = 5;
+
+    //references
+    PlayerMovement player;
+    public Transform tilt;
+    [SerializeField]
+    Camera mainCamera;
+
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerMovement>();
+        //mainCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+
+        transform.position = player.transform.position + Vector3.up * cameraHeight;
+        transform.rotation = player.transform.rotation;
+
+        tilt.eulerAngles = new Vector3(currentTilt, transform.eulerAngles.y, transform.eulerAngles.z);
+        mainCamera.transform.position += tilt.forward * -currenDistance;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        CameraTransforms();
+    }
+
+    void CameraTransforms()
+    {
+        //Cameras rotate is the same as the players
+        currentPan = player.transform.eulerAngles.y;
+        transform.position = player.transform.position + Vector3.up * cameraHeight;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, currentPan, transform.eulerAngles.z);
+        tilt.eulerAngles = new Vector3(currentTilt, tilt.eulerAngles.y, tilt.eulerAngles.z);
+        mainCamera.transform.position = transform.position + tilt.forward * -currenDistance;
     }
 }
