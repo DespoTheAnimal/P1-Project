e25cb0dcd0ff578cf90e8ba50c762feaf1f338b8
LucJakobsen <lucjakobsen@gmail.com>
2021-12-02 12:40:11 +0100

    changed some stuff in homebase 1 scene for dialogue

---
+++ P1-Project/Assets/Scripts/Quests/DialogueManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+    void Awake()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+        
+        titleText.text = dialogue.title;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+
+}
