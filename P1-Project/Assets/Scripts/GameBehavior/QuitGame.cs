f0503c401417be1c704c765e14366b2759d961f7
monniiaa <74655535+monniiaa@users.noreply.github.com>
2021-12-10 22:25:26 +0100

    Ærligt jeg har pushet EditorUserSetting 1000 gange nu så fjerner den altså ikke

---
+++ P1-Project/Assets/Scripts/GameBehavior/QuitGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        QuitApp();
+    }
+
+    void QuitApp()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
